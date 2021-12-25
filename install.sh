7327f8418ef98286fa059f0027612936e8ffe1eb
Jhonathan.H <84613439+JhonathanH@users.noreply.github.com>
2021-12-25 11:43:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,27 +4,11 @@ set -e
 
 #############################################################################
 #                                                                           #
-# Project 'pterodactyl-installer'                                           #
+# Project 'Pterodactyl Français'                                            #
 #                                                                           #
 # Copyright (C) 2018 - 2021, Vilhelm Prytz, <vilhelm@prytznet.se>           #
+# Copyright (C) 2021 - 2022, Jhonathan Hiul, <saispasje24@gmail.com>        #
 #                                                                           #
-#   This program is free software: you can redistribute it and/or modify    #
-#   it under the terms of the GNU General Public License as published by    #
-#   the Free Software Foundation, either version 3 of the License, or       #
-#   (at your option) any later version.                                     #
-#                                                                           #
-#   This program is distributed in the hope that it will be useful,         #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
-#   GNU General Public License for more details.                            #
-#                                                                           #
-#   You should have received a copy of the GNU General Public License       #
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
-#                                                                           #
-# https://github.com/vilhelmprytz/pterodactyl-installer/blob/master/LICENSE #
-#                                                                           #
-# This script is not associated with the official Pterodactyl Project.      #
-# https://github.com/vilhelmprytz/pterodactyl-installer                     #
 #                                                                           #
 #############################################################################
 
